39235e178f2319a17f49e38a4ccb4c628b05633f
lordporya <lordporya2@gmail.com>
2016-02-04 15:03:43 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {130109494},--Sudo users
+    sudo_users = {lordporya 97662544},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
